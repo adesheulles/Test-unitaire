c98d72b41852b579e66de1150c9bd20d5341e5b1
adesheulles <43208125+adesheulles@users.noreply.github.com>
2019-04-03 16:04:47 +0200

    Add files via upload

---
+++ Ean13.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tpTestUnitaire
+{
+    class Ean13
+    {
+        private int[] ean13;
+        public Ean13(int[] ean13)
+        {
+            this.ean13 = new int[12];
+            int k = 0;
+            foreach (int i in ean13)
+            {
+                this.ean13[k] = i;
+                k++;
+            }
+        }
+        public int PoidsImpair()
+        {
+            int poids = 0;
+            for (int i = 0; i < 12; i = i + 2)
+            {
+                poids = poids + this.ean13[i];
+            }
+            return poids;
+        }
+        public int PoidsPair()
+        {
+            int poids = 0;
+            for (int i = 1; i < 13; i = i + 2)
+            {
+                poids = poids + this.ean13[i];
+            }
+            return poids*3;
+        }
+        public int Reste()
+        {
+            int total = PoidsPair() + PoidsImpair();
+            int reste = total % 10;
+            return reste;
+        }
+        public int Cle()
+        {
+            int reste = Reste();
+            if (reste == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 10 - reste;
+            }
+        }
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < 12; i++)
+            {
+                s += this.ean13[i];
+                if (i % 4 == 0)
+                {
+                    s+="-";
+                }
+            }
+            s += Cle();
+            return s;
+        }
+    }
+}
---
+++ Ean13Test.cs
@@ -0,0 +1,138 @@
+﻿using tpTestUnitaire;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace testUnitaire
+{
+    
+    
+    /// <summary>
+    ///Classe de test pour Ean13Test, destinée à contenir tous
+    ///les tests unitaires Ean13Test
+    ///</summary>
+    [TestClass()]
+    public class Ean13Test
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Obtient ou définit le contexte de test qui fournit
+        ///des informations sur la série de tests active ainsi que ses fonctionnalités.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Attributs de tests supplémentaires
+        // 
+        //Vous pouvez utiliser les attributs supplémentaires suivants lorsque vous écrivez vos tests :
+        //
+        //Utilisez ClassInitialize pour exécuter du code avant d'exécuter le premier test dans la classe
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Utilisez ClassCleanup pour exécuter du code après que tous les tests ont été exécutés dans une classe
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Utilisez TestInitialize pour exécuter du code avant d'exécuter chaque test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Utilisez TestCleanup pour exécuter du code après que chaque test a été exécuté
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Test pour Constructeur Ean13
+        ///</summary>
+        [TestMethod()]
+        public void Ean13ConstructorTest()
+        {
+            int[] ean13 = null; // TODO: initialisez à une valeur appropriée
+            Ean13 target = new Ean13(ean13);
+            Assert.Inconclusive("TODO: implémentez le code pour vérifier la cible");
+        }
+
+        /// <summary>
+        ///Test pour Cle
+        ///</summary>
+        [TestMethod()]
+        public void CleTest()
+        {
+            Ean13 target = new Ean13(new int[] { 4, 7, 1, 9, 5, 1, 2, 0, 0, 2, 8, 8 });
+            int expected = 9;
+            int actual = target.Cle();
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///Test pour PoidsImpair
+        ///</summary>
+        [TestMethod()]
+        public void PoidsImpairTest()
+        {
+            Ean13 target = new Ean13(new int[] { 4, 7, 1, 9, 5, 1, 2, 0, 0, 2, 8, 8 });
+            int expected = 20;
+            int actual = target.PoidsImpair();
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///Test pour PoidsPair
+        ///</summary>
+        [TestMethod()]
+        public void PoidsPairTest()
+        {
+            Ean13 target = new Ean13(new int[] { 4, 7, 1, 9, 5, 1, 2, 0, 0, 2, 8, 8 });
+            int expected = 81;
+            int actual = target.PoidsPair();
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///Test pour Reste
+        ///</summary>
+        [TestMethod()]
+        public void ResteTest()
+        {
+            Ean13 target = new Ean13(new int[] { 4, 7, 1, 9, 5, 1, 2, 0, 0, 2, 8, 8 });
+            int expected = 1;
+            int actual = target.Reste();
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///Test pour ToString
+        ///</summary>
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Ean13 target = new Ean13(new int[] { 4, 7, 1, 9, 5, 1, 2, 0, 0, 2, 8, 8 });
+            string expected = "4719-5120-0288-9";
+            string actual = target.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
